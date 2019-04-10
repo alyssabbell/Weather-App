a3f2726599221b223765df05f535a8dd7b270997
belladev <alyssa.bbell@gmail.com>
2019-04-10 13:15:10 -0400

    removed commented out code

--- js/master.js
+++ js/master.js
@@ -1,7 +1,5 @@
   // grab input field control
   let inputField = $("#location");
-  //grab the value from input field
-  //let inputLocation = inputField.val();
 
   // declare function to validate US zip Code
   // returns T or F
@@ -44,7 +42,6 @@
                 url: url,
                 success: function(result){
                   let forecast = result.forecast.forecastday;
-                  console.log(result);
 
                   let $locationH2 = $("<h2/>", { id: "location-banner", text: "7-Day Forecast for " + result.location.name});
 
@@ -62,8 +59,7 @@
                     $article.append($highLowh3);
 
                     $(".cards").append($article);
-
-                  })
+                    })
                 }
             }); // end $.ajax
 
@@ -71,61 +67,3 @@
       }
     }
   });
-
-
-
-
-    //
-    //   if(isValidUSZip(inputField.val()) === true && inputField.val().length === 5)
-    //     {
-    //       let inputLocation = inputField.val();
-    //
-    //       let $main = $("<main/>", {});
-    //       //
-    //       $("header").append($main);
-    //
-    //       $(document).ready( function() {
-    //
-    //         let url = `http://api.apixu.com/v1/forecast.json?key=4227a014893b4e85b70214752190204&q=${inputLocation}&days=7`;
-    //
-    //         $.ajax(
-    //             {
-    //               url: url,
-    //               success: function(result){
-    //                 let forecast = result.forecast.forecastday;
-    //                 //console.log(result);
-    //
-    //                 let $locationH2 = $("<h2/>", { id: "location-banner", text: "7-Day Forecast for " + result.location.name});
-    //                 //$("main").append($locationH2);
-    //
-    //                 $("header").append($locationH2);
-    //
-    //                 $.each (forecast, function(index, dayOfWeek) {
-    //
-    //                   let $highLowh3 = $("<h3/>", { text: dayOfWeek.day.maxtemp_f.toFixed(0) + "º/" + dayOfWeek.day.maxtemp_f.toFixed(0) + "º" });
-    //                   let $iconImg = $("<img/>", { src: "https:" + dayOfWeek.day.condition.icon });
-    //                   let $dateh3 = $("<h3/>", {text: dayOfWeek.date });
-    //                   let $article = $("<article/>", {class: "col-1"});
-    //
-    //                   $article.append($dateh3).css( 'white-space', 'nowrap');
-    //                   $article.append($iconImg);
-    //                   $article.append($highLowh3);
-    //
-    //                   $(".cards").append($article);
-    //
-    //                 })
-    //               }
-    //           }); // end $.ajax
-    //
-    //       });
-    //
-    //
-    //   }
-    //
-    //   else {
-    //
-    //       alert("That is not a valid US Zip Code");
-    //         }
-    //   }
-    //
-    // });
